653d2e083599983247f41bed86a978b88a19d9be
rostiSe <150136554+rostiSe@users.noreply.github.com>
2026-04-03 12:35:04 +0200

    chore: update css entry path, remove deprecated global.css, and upgrade dependencies for react-native-svg and tailwind-merge

--- metro.config.js
+++ metro.config.js
@@ -7,7 +7,7 @@ const config = getDefaultConfig(__dirname);
 
 module.exports = withUniwindConfig(config, {
   // relative path to your global.css file (from previous step)
-  cssEntryFile: "./src/app/global.css",
+  cssEntryFile: "./src/global.css",
   // (optional) path where we gonna auto-generate typings
   // defaults to project's root
   dtsFile: "./src/uniwind-types.d.ts",
