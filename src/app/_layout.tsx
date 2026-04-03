11c2817a356b643379d876a8918d2965612a8de1
rostiSe <150136554+rostiSe@users.noreply.github.com>
2026-04-03 12:35:04 +0200

    chore: update css entry path, remove deprecated global.css, and upgrade dependencies for react-native-svg and tailwind-merge

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,14 @@
 import { Stack } from "expo-router";
-import "./global.css";
+import { HeroUINativeProvider } from "heroui-native/provider";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import "../global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <HeroUINativeProvider>
+        <Stack />
+      </HeroUINativeProvider>
+    </GestureHandlerRootView>
+  );
 }
